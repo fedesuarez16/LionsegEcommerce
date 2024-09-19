4b65c4bf78161fbbf43e0d011d57fc29ac1699e2
fedesuarez16 <fedesuarez16@gmail.com>
2024-09-18 23:04:53 -0300

    feat: idk

--- src/app/(store)/page.tsx
+++ src/app/(store)/page.tsx
@@ -1,15 +1,13 @@
+import Image from "next/image";
 import type { Metadata } from "next/types";
 import * as Commerce from "commerce-kit";
-import Link from "next/link";
-import Image from "next/image";
-import Logo from "./logo.png";
+import ImageOne from "./img.png";
 import { ProductList } from "@/ui/products/productList";
 import { CategoryBox } from "@/ui/CategoryBox";
-import Mesa from "@/images/mesa.png";
-import Sillon from "@/images/sillones.png";
-import Respaldos from "@/images/respaldos.png";
+import AccessoriesImage from "@/images/accessories.png";
+import ApparelImage from "@/images/electrodomesticos.png";
+import { YnsLink } from "@/ui/YnsLink";
 import { publicUrl } from "@/env.mjs";
-import Envios from "@/app/(store)/Envios";
 
 export const metadata = {
 	alternates: { canonical: publicUrl },
@@ -20,51 +18,45 @@ export default async function Home() {
 
 	return (
 		<main>
-			<section className="relative w-full rounded-lg">
-				<video
-					src="/banner.mp4"
-					width="1200"
-					height="100"
-					autoPlay
-					loop
-					muted
-					playsInline
-					className="h-[400px] w-full rounded-lg object-cover object-center"
-					style={{ aspectRatio: "1200/600", objectFit: "cover" }}
-				/>
-				<div className="absolute inset-0 flex flex-col items-center justify-center bg-gradient-to-t from-[rgba(0,0,0,0.9)] to-[rgba(0,0,0,0.3)] px-4 md:px-6">
-					<div className="space max-w-2xl text-center">
-						<Image
-							alt="idk"
-							src={Logo}
-							width={250}
-							height={160}
-							className="mx-auto object-contain text-xl font-bold"
-						/>
-						<p className="px-4 text-lg tracking-tighter text-gray-300 md:text-xl">
-							Aprovecha un 30% de descuento con transferencia en todos nuestros productos.
+			<section className="rounded bg-neutral-100 py-8 sm:py-12">
+				<div className="mx-auto grid grid-cols-1 items-center justify-items-center gap-8 px-8 sm:px-16 md:grid-cols-2">
+					<div className="max-w-md space-y-4">
+						<h2 className="text-balance text-3xl font-bold tracking-tight md:text-4xl">
+							Descubri la coleccion de productos de{" "}
+							<p className="text-indigo-400">Tiendacompleta</p>
+						</h2>
+						<p className="text-pretty text-neutral-600">
+							Explora nuestra seleccion de productos y encontra lo que estas buscando{" "}
 						</p>
-						<Link
+						<YnsLink
+							className="inline-flex h-10 items-center justify-center rounded-full bg-neutral-900 px-6 font-medium text-neutral-50 transition-colors hover:bg-neutral-900/90 focus:outline-none focus:ring-1 focus:ring-neutral-950"
 							href="/products"
-							className="my-4 inline-flex h-10 items-center justify-center rounded-md bg-primary px-8 text-sm font-medium text-primary-foreground shadow transition-colors hover:bg-primary/90 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50"
-							prefetch={false}
 						>
-							Ver productos
-						</Link>
+							Compra ahora
+						</YnsLink>
 					</div>
+					<Image
+						alt="Cup of coffee"
+						loading="eager"
+						priority={true}
+						className="rounded"
+						height={450}
+						width={450}
+						src={ImageOne}
+						style={{
+							objectFit: "cover",
+						}}
+						sizes="(max-width: 640px) 70vw, 450px"
+					/>
 				</div>
 			</section>
-
-			<Envios />
-
 			<ProductList products={products} />
 
 			<section className="w-full py-8">
-				<div className="grid gap-8 lg:grid-cols-3">
+				<div className="grid gap-8 lg:grid-cols-2">
 					{[
-						{ categorySlug: "sillas", src: Mesa },
-						{ categorySlug: "sillones", src: Sillon },
-						{ categorySlug: "respaldos", src: Respaldos },
+						{ categorySlug: "accesorios", src: AccessoriesImage },
+						{ categorySlug: "electrodomesticos", src: ApparelImage },
 					].map(({ categorySlug, src }) => (
 						<CategoryBox key={categorySlug} categorySlug={categorySlug} src={src} />
 					))}
